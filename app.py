5bf3e34a9fa6173202405ffeea5e1dd8a13d2e70
Hocine Brahiti <brahiti_hocine@yahoo.fr>
2025-08-01 21:08:33 +0200

    MAJ

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from agent import ask_openrouter
 st.title("Ma première application IA ")
 
 
-question = st.text_input("Posez votre question, je suis là pour vous répondre :")
+question = st.text_input("Bonjour M. Chef GRADEL, Posez votre question, je suis là pour vous répondre :")
 
 if st.button("Envoyer") and question:
     response = ask_openrouter(question)
